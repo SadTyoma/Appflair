80eeba5bc3b8c9832afc35409ac042d90a2161fd
Sad Tyoma <artyomshuneyko@gmail.com>
2023-07-05 16:14:35 +0300

    Initial Commit

---
+++ Appflair/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appflair
+//
+//  Created by Artem Shuneyko on 5.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
